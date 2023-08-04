b26060bddf1dfbc2c55f5585eb99ab430b4fc72a
Jishap-Tomsher <jishap.tomsher@gmail.com>
2023-08-04 09:53:33 +0400

    Designs

--- resources/views/web/user/reschedule_data.blade.php
+++ resources/views/web/user/reschedule_data.blade.php
@@ -1,25 +1,28 @@
-    @php 
+@php 
         $RequestedPreferences = $PtrDetails['RequestedPreferences'];
         $arrrowImg = asset('assets/img/icon/right_arrow.png');
     @endphp 
     @foreach($RequestedPreferences as $pref)
-        <div class="flight_search_items">
-            <div class="multi_city_flight_lists">
+        <div class="flight_search_items mt-3">
+            <div class="multi_city_flight_lists pt-3">
                 @foreach($pref['QuotedSegments'] as $segment)
                     @php
                         $airlineData = getAirlineData($segment['AirlineCode']);
                         $deptAirportData = getAirportData($segment['DepartureAirportLocationCode']);
                         $arrAirportData = getAirportData($segment['ArrivalAirportLocationCode']);
                     @endphp
-                    <div class="flight_multis_area_wrapper">
-                        <div class="flight_search_left">
-                            <div class="flight_logo">
+                    
+                    <div class="flight_multis_area_wrapper pb-4">
+                        
+                        <div class="flight_logo">
                                 <img src="{{ isset($airlineData[0]) ? $airlineData[0]['AirLineLogo'] : '' }}" alt="img">
                                 <div class="flight-details">
                                     <h4> {{ isset($airlineData[0]) ? $airlineData[0]['AirLineName'] : ''}} </h4>
                                     <h6> {{ $segment['FlightNumber']  }}</h6>
                                 </div>
                             </div>
+                        <div class="flight_search_left">
+                            
                             <div class="flight_search_destination">
                                 <p>From</p>
                                 <span>{{ date('d M, Y', strtotime($segment['DepartureDateTime'])) }}</span>
@@ -35,7 +38,11 @@
                             
                                 <p>{{ convertToHoursMins($segment['JourneyDuration']) }} </p>
                             </div>
-                            <div class="flight_search_destination">
+                           
+                        </div>
+                        
+                        <div class="flight_search_third">
+                         <div class="flight_search_destination">
                                 <p>To</p>
                                 <span>{{ date('d M, Y', strtotime($segment['ArrivalDateTime'])) }}</span>
                                 <h2>{{ date('H:i', strtotime($segment['ArrivalDateTime'])) }} </h2>
@@ -43,7 +50,11 @@
                                 <h6>{{ $arrAirportData[0]['AirportName'] }} </h6>
                             </div>
                         </div>
+                        
                     </div>
+                    
+                    
+                    
                 @endforeach
                 @php
                     $fareDiff = 0;
@@ -53,11 +64,13 @@
                         $currency = $fares['TotalFareDifference']['CurrencyCode'];
                     }
                 @endphp 
-                <div class="flight_search_right">
+                
+            </div>
+            
+            <div class="flight_search_right  text-center h-100">
                     <h4> Total fare difference </h4>
                     <h2>{{ $currency }} {{ $fareDiff }} </h2>
-                    <button type="submit" class="btn btn_theme btn_lg mt-10 reissueButton" data-uniqueID="{{ $uniqueBookId }}" data-ptrUniqueID="{{ $ptrUniqueId }}" data-currency="{{ $currency }}" data-fare="{{ $fareDiff }}" data-preference="{{ $pref['PreferenceOption'] }}" name="reissueButton" id="reissueButton">Send Reschedule Request</button>
+                    <button type="submit" class="btn btn_theme btn_lg mt-10 reissueButton" data-uniqueID="{{ $uniqueBookId }}" data-ptrUniqueID="{{ $ptrUniqueId }}" data-currency="{{ $currency }}" data-fare="{{ $fareDiff }}" data-preference="{{ $pref['PreferenceOption'] }}" name="reissueButton" id="reissueButton">Send Request</button>
                 </div>
-            </div>
         </div>
     @endforeach
